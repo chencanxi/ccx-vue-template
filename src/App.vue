54be9265e9327ea1dd486d1fdb933c46850b6104
chencanxi <chencanxi@zhiketong.cn>
2025-01-05 22:21:26 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div style="padding: 20px">
+    <h1>CCX Vue 3 + Vite (JS)</h1>
+    <p>Hello, Vue 3 in JavaScript!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+/* 这里可以放置一些全局样式或 scoped 样式 */
+</style>
